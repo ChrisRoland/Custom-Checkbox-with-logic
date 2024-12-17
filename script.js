3f0f2566ac70fa343c57c2be9cbb27dbb15e2a82
ChrisRoland <chrisebuberoland@gmail.com>
2024-12-17 11:30:58 +0100

    feat: complete

---
+++ script.js
@@ -0,0 +1,22 @@
+const toggles = document.querySelectorAll(".toggle")
+const good = document.getElementById("good")
+const cheap = document.getElementById("cheap")
+const fast = document.getElementById("fast")
+
+toggles.forEach(toggle => toggle.addEventListener("change", (e) => logic(e.target)))
+
+function logic(clicked) { 
+    if(good.checked && cheap.checked && fast.checked) {
+        if(good === clicked) {
+            fast.checked = false
+        }
+
+        if(cheap === clicked) {
+            good.checked = false
+        }
+
+        if(fast === clicked) {
+            cheap.checked = false
+        }
+    }
+ }
